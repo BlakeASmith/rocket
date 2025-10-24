790e6990187810a23ee5d93ae1d2677547a12d42
Blake Smith <blake@Mac.lan>
2025-10-23 22:56:46 -0700

    chore: remove redundant paths

--- cmd/goto.go
+++ cmd/goto.go
@@ -2,8 +2,6 @@ package cmd
 
 import (
 	"fmt"
-	"os/exec"
-	"path/filepath"
 	"strings"
 
 	"github.com/spf13/cobra"
@@ -25,40 +23,6 @@ Once selected, it prints "cd <path>" to stdout, which your shell can execute to 
 func RunGoto(cmd *cobra.Command, args []string) {
 	rocketRoot := GetRocketRoot()
 
-	if len(args) == 0 {
-		// Original behavior: list all and fzf
-		matches := getMatchingDirs(rocketRoot, "")
-		if len(matches) == 0 {
-			return
-		}
-
-		// Compute relatives for fzf display
-		relatives := make([]string, len(matches))
-		for i, match := range matches {
-			rel, _ := filepath.Rel(rocketRoot, match)
-			relatives[i] = rel
-		}
-
-		fzfCmd := exec.Command("fzf")
-		fzfCmd.Stdin = strings.NewReader(strings.Join(relatives, "\n"))
-
-		output, err := fzfCmd.Output()
-		if err != nil {
-			return
-		}
-
-		selectedRel := strings.TrimSpace(string(output))
-		// Find the corresponding absolute and print it
-		for i, rel := range relatives {
-			if rel == selectedRel {
-				fmt.Println(matches[i])
-				return
-			}
-		}
-		return
-	}
-
-	// Fuzzy match mode
 	query := strings.Join(args, " ")
 	selected := selectDir(rocketRoot, query)
 	if selected != "" {
--- cmd/rm.go
+++ cmd/rm.go
@@ -3,7 +3,6 @@ package cmd
 import (
 	"fmt"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"strings"
 
@@ -24,42 +23,8 @@ Once selected, it deletes the directory.`,
 func RunRm(cmd *cobra.Command, args []string) {
 	rocketRoot := GetRocketRoot()
 
-	var selected string
-	if len(args) == 0 {
-		// Original behavior: list all and fzf
-		matches := getMatchingDirs(rocketRoot, "")
-		if len(matches) == 0 {
-			return
-		}
-
-		// Compute relatives for fzf display
-		relatives := make([]string, len(matches))
-		for i, match := range matches {
-			rel, _ := filepath.Rel(rocketRoot, match)
-			relatives[i] = rel
-		}
-
-		fzfCmd := exec.Command("fzf")
-		fzfCmd.Stdin = strings.NewReader(strings.Join(relatives, "\n"))
-
-		output, err := fzfCmd.Output()
-		if err != nil {
-			return
-		}
-
-		selectedRel := strings.TrimSpace(string(output))
-		// Find the corresponding absolute
-		for i, rel := range relatives {
-			if rel == selectedRel {
-				selected = matches[i]
-				break
-			}
-		}
-	} else {
-		// Fuzzy match mode
-		query := strings.Join(args, " ")
-		selected = selectDir(rocketRoot, query)
-	}
+	query := strings.Join(args, " ")
+	selected := selectDir(rocketRoot, query)
 
 	if selected == "" {
 		return
